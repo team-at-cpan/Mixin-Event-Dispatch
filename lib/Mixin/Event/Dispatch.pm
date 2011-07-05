4dce0b34eba8f1a24b26ec3b0a9d33aca2d17c7f
Tom Molesworth <tom@perlsite.co.uk>
2011-07-05 06:15:28 +0000

    Fix missing parent dep, add note on roles and fill out docs slightly
    git-svn-id: http://entitymodel.com/repo/trunk/Mixin-Event-Dispatch@70 c09ddf91-2ef0-45e8-8c68-47ccf8e3121d

--- lib/Mixin/Event/Dispatch.pm
+++ lib/Mixin/Event/Dispatch.pm
@@ -24,7 +24,7 @@ Mixin::Event::Dispatch - mixin methods for simple event/message dispatch framewo
 	my $self = shift;
 	my %args = @_;
 	$self->add_handler_for_event(
-		map { (/^on_(.*)$/) ? ($1 => $args{$_}) : () } for keys %args
+		map { (/^on_(.*)$/) ? ($1 => $args{$_}) : () } keys %args
 	);
 	return $self;
  }
@@ -32,6 +32,24 @@ Mixin::Event::Dispatch - mixin methods for simple event/message dispatch framewo
 =head1 DESCRIPTION
 
 Add this in as a parent to your class, and it'll provide some methods for defining event handlers (L</add_event_handler>) and calling them (L</invoke_event>).
+
+Note that handlers should return 0 for a one-off handler, and 1 if it should be called again on the next event.
+
+=head1 SPECIAL EVENTS
+
+A single event has been reserved for cases where a callback dies:
+
+=over 4
+
+=item * C< event_error > - if a handler is available, this will be called instead of dying whenever any other handler dies. If an C< event_error > handler also fails,
+then this error will be re-thrown. As with the other handlers, you can have more than one C< event_error > handler.
+
+=back
+
+=head1 API HISTORY
+
+Note that 0.002 changed to use L<event_handlers> instead of C< event_stack > for storing the available handlers (normally only L<invoke_event> and
+L<add_handler_for_event> are expected to be called directly).
 
 =head1 METHODS
 
@@ -65,9 +83,9 @@ sub invoke_event {
 		}
 	};
 
-	my $stack = $self->event_handlers;
+	my $handlers = $self->event_handlers;
 # If we have handlers for this event, use them directly.
-	if($stack && scalar @{$stack->{$ev} || [] }) {
+	if($handlers && scalar @{$handlers->{$ev} || [] }) {
 		# Run all the queued code for this event, removing the handlers that return false.
 		List::UtilsBy::extract_by { $run_event->($_) } @{$self->event_handlers->{$ev}};
 		return $self;
@@ -133,6 +151,13 @@ sub clear_event_handlers {
 
 __END__
 
+=head1 WHY NOT A ROLE?
+
+Most role systems should be able to use this class - either directly, or through a thin wrapper which adds
+any required boilerplate. Try L<Moose> or L<Role::Tiny> / L<Moo::Role> for that.
+
+Alternatively, you could use this as a component via L<Class::C3::Componentised>.
+
 =head1 SEE ALSO
 
 There are at least a dozen similar modules already on CPAN, eventually I'll add a list of them here.
@@ -140,6 +165,9 @@ There are at least a dozen similar modules already on CPAN, eventually I'll add 
 =head1 AUTHOR
 
 Tom Molesworth <cpan@entitymodel.com>
+
+with thanks to various helpful people on freenode #perl who suggested making L</event_handlers> into an
+accessor (to support non-hashref objects) and who patiently tried to explain about roles.
 
 =head1 LICENSE
 
